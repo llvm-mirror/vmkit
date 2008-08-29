7f8320634b6464920abc6964b90db32def34f252
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2008-08-29 07:41:47 +0000

    Ooops, forfot to commit that file.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/branches/isolate@55532 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ lib/JnJVM/VMCore/JnjvmConfig.h
@@ -0,0 +1,36 @@
+//===---------- JnjvmConfig.h - Jnjvm configuration file ------------------===//
+//
+//                              JnJVM
+//
+// This file is distributed under the University of Illinois Open Source 
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+
+#ifndef JNJVM_CONFIG_H
+#define JNJVM_CONFIG_H
+
+#ifdef MULTIPLE_VM
+#define ISOLATE_STATIC
+#else
+
+namespace jnjvm {
+
+class ClassArray;
+class ClassPrimitive;
+class Class;
+class CommonClass;
+class JavaConstantPool;
+
+#define UserClassArray ClassArray
+#define UserClassPrimitive ClassPrimitive
+#define UserClass Class
+#define UserCommonClass CommonClass
+#define UserConstantPool JavaConstantPool
+
+}
+#define ISOLATE_STATIC static
+#endif
+
+#endif // JNJVM_CONFIG_H
