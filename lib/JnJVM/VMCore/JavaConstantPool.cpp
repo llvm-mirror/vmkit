3883d81a27649cc97501042a289583c843686901
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2008-10-27 17:30:29 +0000

    Don't lookup twice when it's the bootstrap loader.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@58254 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/JnJVM/VMCore/JavaConstantPool.cpp
+++ lib/JnJVM/VMCore/JavaConstantPool.cpp
@@ -306,7 +306,7 @@ CommonClass* JavaConstantPool::getMethodClassIfLoaded(uint32 index) {
     assert(loader && "Class has no loader?");
     const UTF8* name = UTF8At(ctpDef[index]);
     temp = loader->lookupClass(name);
-    if (!temp) 
+    if (!temp && loader != loader->bootstrapLoader)
       temp = loader->bootstrapLoader->lookupClass(name);
   }
 #endif
