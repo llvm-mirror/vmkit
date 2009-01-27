fa1beeb0af980cc9cf8214a4d8786393e2d7b927
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-01-27 13:05:32 +0000

    untabify
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@63102 91177308-0d34-0410-b5e6-96231b3b80d8

--- tools/vmkit/Launcher.cpp
+++ tools/vmkit/Launcher.cpp
@@ -70,7 +70,7 @@ inline void addPass(FunctionPassManager *PM, Pass *P) {
 void addCommandLinePass(mvm::CompilationUnit* CU, char** argv) {
   FunctionPassManager* Passes = CU->TheModule->globalFunctionPasses;
 
-	Passes->add(new TargetData(*CU->TheModule->TheTargetData));
+  Passes->add(new TargetData(*CU->TheModule->TheTargetData));
   // Create a new optimization pass for each one specified on the command line
   for (unsigned i = 0; i < PassList.size(); ++i) {
     // Check to see if -std-compile-opts was specified before this option.  If
