f4b5ab5a59cc87472bda0a0db9221e9ccbcd0f07
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-03-19 10:15:18 +0000

    Fix includes.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@67299 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/JnJVM/VMCore/Reader.cpp
+++ lib/JnJVM/VMCore/Reader.cpp
@@ -7,8 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
 #include "types.h"
 
