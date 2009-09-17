bddea058987f540f574e1746c4e3454be16fc6a5
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-09-17 09:31:42 +0000

    Set the mvm::MvmModule::GC strategy when it's created.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@82141 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Mvm/Compiler/VmkitGC.cpp
+++ lib/Mvm/Compiler/VmkitGC.cpp
@@ -12,6 +12,8 @@
 #include "llvm/CodeGen/GCStrategy.h"
 #include "llvm/Support/Compiler.h"
 
+#include "mvm/JIT.h"
+
 using namespace llvm;
 
 namespace {
@@ -30,4 +32,5 @@ X("vmkit", "VMKit GC for JIT-generated functions");
 
 VmkitGC::VmkitGC() {
   NeededSafePoints = 1 << GC::PostCall;
+  mvm::MvmModule::GC = this;
 }
