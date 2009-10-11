1bdddcfafc6a06b41c21cac9216425f3d3b63426
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-10-11 17:32:35 +0000

    Add include.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@83775 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Mvm/Compiler/EscapeAnalysis.cpp
+++ lib/Mvm/Compiler/EscapeAnalysis.cpp
@@ -18,6 +18,7 @@
 #include "llvm/Support/CallSite.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include <cstddef>
 #include <map>
