ba58922048936af213d5f52dfe10cf1e4ad764c0
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-10-26 12:48:59 +0000

    Remove VISIBILITY_HIDDEN macro in .cpp file.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@85115 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Mvm/StaticGCPass/StaticGCPass.cpp
+++ lib/Mvm/StaticGCPass/StaticGCPass.cpp
@@ -18,7 +18,7 @@ using namespace llvm;
 
 namespace {
 
-  class VISIBILITY_HIDDEN StaticGCPass : public ModulePass {
+  class StaticGCPass : public ModulePass {
   public:
     static char ID;
     
