62affa4f54292f53775fc3d1c98ba2452ef0b554
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-10-30 09:20:31 +0000

    Bugfix when setting the available byte.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@85584 91177308-0d34-0410-b5e6-96231b3b80d8

--- mmtk/mmtk-j3/ObjectModel.cpp
+++ mmtk/mmtk-j3/ObjectModel.cpp
@@ -45,9 +45,12 @@ extern "C" uint8_t Java_org_j3_mmtk_ObjectModel_readAvailableByte__Lorg_vmmagic_
 }
 
 extern "C" void Java_org_j3_mmtk_ObjectModel_writeAvailableByte__Lorg_vmmagic_unboxed_ObjectReference_2B (JavaObject* OM, JavaObject* obj, uint8_t val) {
+  assert((val & mvm::GCMask) == val && "GC bits do not fit");
 #if defined(__PPC__)
+  ((uint8_t*)obj)[7] &= ~mvm::GCMask;
   ((uint8_t*)obj)[7] |= val;
 #else
+  ((uint8_t*)obj)[4] &= ~mvm::GCMask;
   ((uint8_t*)obj)[4] |= val;
 #endif
 }
@@ -64,6 +67,8 @@ extern "C" uint8_t
 Java_org_j3_mmtk_ObjectModel_attemptAvailableBits__Lorg_vmmagic_unboxed_ObjectReference_2Lorg_vmmagic_unboxed_Word_2Lorg_vmmagic_unboxed_Word_2(
     JavaObject* OM, JavaObject* obj, intptr_t oldValue, intptr_t newValue) {
   
+  assert(((oldValue & ~mvm::GCMask) == (newValue & ~mvm::GCMask)) &&
+         "GC bits do not fit");
   return __sync_bool_compare_and_swap(((intptr_t*)obj) + 1, oldValue, newValue);
 }
 
