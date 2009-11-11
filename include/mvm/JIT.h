331b32d9e1697ebfea81b69b72ef2ef26a914950
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2009-11-11 16:50:37 +0000

    Forgot that file from last commit.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@86842 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/mvm/JIT.h
+++ include/mvm/JIT.h
@@ -184,7 +184,7 @@ public:
    static llvm::ExistingModuleProvider *globalModuleProvider;
    static llvm::FunctionPassManager* globalFunctionPasses;
    static const llvm::TargetData* TheTargetData;
-   static mvm::BumpPtrAllocator Allocator;
+   static mvm::BumpPtrAllocator* Allocator;
 
    static uint64 getTypeSize(const llvm::Type* type);
    static void runPasses(llvm::Function* func, llvm::FunctionPassManager*);
