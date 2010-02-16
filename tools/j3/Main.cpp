f06ce87b12a9736c2ef02b2d805666e2e9aaabdc
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2010-02-16 08:27:58 +0000

    Stop being generic when creating the VM.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@96338 91177308-0d34-0410-b5e6-96231b3b80d8

--- tools/j3/Main.cpp
+++ tools/j3/Main.cpp
@@ -1,4 +1,4 @@
-//===--------- Main.cpp - Simple execution of JnJVM -----------------------===//
+//===------------- Main.cpp - Simple execution of J3 ----------------------===//
 //
 //                          The VMKit project
 //
@@ -14,26 +14,39 @@
 #include "mvm/Threads/Thread.h"
 
 #include "j3/JavaJITCompiler.h"
+#include "../../lib/J3/VMCore/JnjvmClassLoader.h"
+#include "../../lib/J3/VMCore/Jnjvm.h"
 
 #include "llvm/Support/ManagedStatic.h"
 
 
 using namespace j3;
-using namespace llvm;
 using namespace mvm;
 
 int main(int argc, char **argv, char **envp) {
   llvm::llvm_shutdown_obj X;  
-    
+
+  // Initialize base components.  
   MvmModule::initialise();
   Collector::initialise();
+  
+  // Tell the compiler to run all optimizations.
+  MvmModule::AddStandardCompilePasses();
  
+  // Create the allocator that will allocate the bootstrap loader and the JVM.
+  mvm::BumpPtrAllocator Allocator;
   JavaJITCompiler* Comp = JavaJITCompiler::CreateCompiler("JITModule");
-  mvm::MvmModule::AddStandardCompilePasses();
-  JnjvmClassLoader* JCL = VirtualMachine::initialiseJVM(Comp);
-  VirtualMachine* vm = VirtualMachine::createJVM(JCL);
+  JnjvmBootstrapLoader* loader = new(Allocator, "Bootstrap loader")
+    JnjvmBootstrapLoader(Allocator, Comp, true);
+  Jnjvm* vm = new(Allocator, "VM") Jnjvm(Allocator, loader);
+ 
+  // Run the application. 
   vm->runApplication(argc, argv);
   vm->waitForExit();
+
+  // Destroy everyone.
+  vm->~Jnjvm();
+  loader->~JnjvmBootstrapLoader();
 
   return 0;
 }
