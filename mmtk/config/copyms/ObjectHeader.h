d75d8802fdd59fd17bd0bc65018c098b10a84198
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2010-08-01 14:45:04 +0000

    New supported collector: copyms.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@109981 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ mmtk/config/copyms/ObjectHeader.h
@@ -0,0 +1,37 @@
+//===----- ObjectHeader.h - Macros for describing an object header --------===//
+//
+//                          The VMKit project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MVM_OBJECT_HEADER_H
+#define MVM_OBJECT_HEADER_H
+
+#include <stdint.h>
+
+namespace mvm {
+#if (__WORDSIZE == 64)
+  static const uint64_t FatMask = 0x8000000000000000;
+#else
+  static const uint64_t FatMask = 0x80000000;
+#endif
+
+  static const uint64_t ThinCountMask = 0xFF000;
+  static const uint64_t ThinCountShift = 12;
+  static const uint64_t ThinCountAdd = 0x1000;
+
+  static const uint64_t NonLockBitsMask = 0xFFF;
+  static const uint64_t HashMask = 0xFF0;
+  static const uint64_t GCBitMask = 0xF;
+
+  static const uint32_t NonLockBits = 12;
+  static const uint32_t HashBits = 8;
+  static const uint32_t GCBits = 4;
+
+  static const bool MovesObject = true;
+}
+
+#endif // MVM_OBJECT_HEADER_H
