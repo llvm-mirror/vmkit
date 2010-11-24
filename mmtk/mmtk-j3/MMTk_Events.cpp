4fa899bf443d74a0f70edae52910b5bf2fdef1d4
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2010-11-24 22:05:22 +0000

    DEBUG is defined debug.h with ENABLE_OPTIMIZED=1. Change to if 0.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/branches/precise@120127 91177308-0d34-0410-b5e6-96231b3b80d8

--- mmtk/mmtk-j3/MMTk_Events.cpp
+++ mmtk/mmtk-j3/MMTk_Events.cpp
@@ -17,21 +17,21 @@ using namespace j3;
 
 extern "C" void Java_org_j3_mmtk_MMTk_1Events_tracePageAcquired__Lorg_mmtk_policy_Space_2Lorg_vmmagic_unboxed_Address_2I(
     JavaObject* event, JavaObject* space, uintptr_t address, int numPages) {
-#ifdef DEBUG
+#if 0
   fprintf(stderr, "Pages acquired by thread %p from space %p at %x (%d)\n", (void*)mvm::Thread::get(), (void*)space, address, numPages);
 #endif
 }
 
 extern "C" void Java_org_j3_mmtk_MMTk_1Events_tracePageReleased__Lorg_mmtk_policy_Space_2Lorg_vmmagic_unboxed_Address_2I(
     JavaObject* event, JavaObject* space, uintptr_t address, int numPages) {
-#ifdef DEBUG
+#if 0
   fprintf(stderr, "Pages released by thread %p from space %p at %x (%d)\n", (void*)mvm::Thread::get(), (void*)space, address, numPages);
 #endif
 }
 
 extern "C" void Java_org_j3_mmtk_MMTk_1Events_heapSizeChanged__Lorg_vmmagic_unboxed_Extent_2(
     JavaObject* event, uintptr_t heapSize) {
-#ifdef DEBUG
+#if 0
   fprintf(stderr, "New heap size : %d\n", (int)heapSize);
 #endif
 }
