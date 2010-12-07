d7326818337240cf4214eaff3bf4beb36830bd43
Gael Thomas <gael.thomas@lip6.fr>
2010-12-07 15:15:00 +0000

    add a find-it.sh tool able to find a string in vmkit
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/branches/multi-vm@121136 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ find-it.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+
+		echo $0 pattern
+
+else
+
+		grep -w --exclude *.txt --exclude-dir autoconf --exclude config.status --exclude config.log --exclude configure --exclude *.xml --exclude *.html --exclude *.jar --exclude *.bc --exclude MMTkInline.inc --exclude-dir patches --exclude-dir N3 --exclude *.java --exclude *.class -R --exclude-dir .svn --exclude-dir Release --exclude *.s "$1" .
+
+fi
