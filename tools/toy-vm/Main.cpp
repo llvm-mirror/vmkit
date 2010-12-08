ec3cc30bc03322ec1d0dbc8ad0caed68404a4bc7
Gael Thomas <gael.thomas@lip6.fr>
2010-12-08 15:11:59 +0000

    create the class toy vm
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/branches/multi-vm@121261 91177308-0d34-0410-b5e6-96231b3b80d8

--- tools/toy-vm/Main.cpp
+++ tools/toy-vm/Main.cpp
@@ -1,5 +1,36 @@
+#include "mvm/VMKit.h"
+#include "mvm/VirtualMachine.h"
+#include "mvm/Threads/Thread.h"
 
+#define NI() fprintf(stderr, "Not implemented\n"); abort()
+
+namespace toyvm {
+
+	class ToyVM : public mvm::VirtualMachine {
+	public:
+		ToyVM(mvm::BumpPtrAllocator &alloc, mvm::VMKit* vmkit) : mvm::VirtualMachine(alloc, vmkit) {}
+
+		virtual mvm::gc** getReferent(mvm::gc*) { NI(); }
+		virtual void setReferent(mvm::gc*, mvm::gc*) { NI(); }
+		virtual bool enqueueReference(mvm::gc*) { NI(); }
+		virtual size_t getObjectSize(mvm::gc*) { NI(); }
+		virtual void runApplication(int, char**) { NI(); }
+		virtual void waitForExit() { NI(); }
+	};
+
+	class ToyVMThread : public mvm::VMThreadData {
+	};
+
+}
+
+using namespace toyvm;
 
 int main(int argc, char **argv) {
+  // Initialize base components.  
+  mvm::BumpPtrAllocator Allocator;
+	mvm::VMKit* vmkit = new(Allocator, "VMKit") mvm::VMKit(Allocator);
+
+	new(Allocator, "toy vm") ToyVM(Allocator, vmkit);
+
 	return 0;
 }
