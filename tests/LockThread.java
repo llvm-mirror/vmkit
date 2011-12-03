d6897803eceb52fa6a669a603f8087349bb25222
Will Dietz <wdietz2@illinois.edu>
2011-12-03 10:44:46 +0000

    Simplify LockThread test: force too many fatlocks to be alloc'd, VM segfaults.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@145768 91177308-0d34-0410-b5e6-96231b3b80d8

--- tests/LockThread.java
+++ tests/LockThread.java
@@ -1,29 +1,26 @@
-// Some madness with threads and locks.  Shouldn't cause VM to crash!
+// Allocate too many fat locks, crash VM
 public class LockThread extends Thread {
   static Object Lock;
   static long x = 0;
-  static final long max = 1000000;
+  static final long max = 1000;
   public void run() {
     while(x < max)
       synchronized(Lock) {
         ++x;
-        if (x % 100 == 0)
-          ++x;
       }
   }
 
   public static void main(String[] args) {
-    Lock = new Object();
-    for(int iter = 0; iter < 100; ++iter) {
-      x = 0;
-      for(int i = 0; i < 100; ++i) {
-        new LockThread().start();
-      }
-      while(x < max)
-        Lock = new Object();
-      try {
-        sleep(100);
-      } catch(InterruptedException ignored);
+    for(int i = 0; i < 2048; ++i) {
+      Lock = new Object();
+
+      Thread t1 = new LockThread();
+      Thread t2 = new LockThread();
+      t1.start();
+      t2.start();
+
+      try { t1.join(); } catch (InterruptedException ignore){}
+      try { t2.join(); } catch (InterruptedException ignore){}
     }
   }
 }
