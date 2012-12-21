27d8d563a9d470f31cbc4076d39cd51b2f4b9e9f
Nicolas Geoffray <nicolas.geoffray@gmail.com>
2012-12-21 22:44:29 +0000

    Bugfix when a non-customized version was becoming a customized one. If the LLVMInfo does not know if the method is customizable, don't try to customize it.
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@170943 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/j3/Compiler/JavaLLVMCompiler.cpp
+++ lib/j3/Compiler/JavaLLVMCompiler.cpp
@@ -61,7 +61,7 @@ Function* JavaLLVMCompiler::parseFunction(JavaMethod* meth, Class* customizeFor)
   // We are jitting. Take the lock.
   vmkit::VmkitModule::protectIR();
   if (func->getLinkage() == GlobalValue::ExternalWeakLinkage) {
-    JavaJIT jit(this, meth, func, customizeFor);
+    JavaJIT jit(this, meth, func, LMI->isCustomizable? customizeFor : NULL);
     if (isNative(meth->access)) {
       jit.nativeCompile();
       vmkit::VmkitModule::runPasses(func, JavaNativeFunctionPasses);
@@ -75,11 +75,9 @@ Function* JavaLLVMCompiler::parseFunction(JavaMethod* meth, Class* customizeFor)
     if (!LMI->isCustomizable && jit.isCustomizable) {
       // It's the first time we parsed the method and we just found
       // out it can be customized.
+      // TODO(geoffray): return a customized version to this caller.
       meth->isCustomizable = true;
       LMI->isCustomizable = true;
-      if (customizeFor != NULL) {
-        LMI->setCustomizedVersion(customizeFor, func);
-      }
     }
   }
   vmkit::VmkitModule::unprotectIR();
