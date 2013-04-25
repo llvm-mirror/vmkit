1963c48149ec11aa5cc014a5daa4b418f9f5d09c
Peter Senna Tschudin <peter.senna@gmail.com>
2013-04-25 17:04:07 +0000

    Reactiating debug.h You need only to set 0 to 1 in line 67 of debug.h (cherry picked from commit efa5370fc1596a7b083420e0f18dde1eee86cc3f)
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@180385 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/debug.h
+++ include/debug.h
@@ -14,19 +14,22 @@
 #define COLOR_NORMAL ""
 #define END "m"
 
-#define _BLACK "00"
-#define _RED "01"
-#define _GREEN "02"
-#define _YELLOW "03"
-#define _BLUE "04"
-#define _MAGENTA "05"
-#define _CYAN "06"
-#define _WHITE "07"
+#define _BLACK "30"
+#define _RED "31"
+#define _GREEN "32"
+#define _YELLOW "33"
+#define _BLUE "34"
+#define _MAGENTA "35"
+#define _CYAN "36"
+#define _WHITE "37"
 
 #define _NORMAL "00"
 #define _BOLD "01"
 #define _SOULIGNE "04"
 
+#define MK_COLOR(type, bg) type";"bg
+
+/*
 #define MK_COLOR(type, bg, fg) type";"bg";"fg
 
 #define WHITE MK_COLOR(_NORMAL, _BLACK, _BLACK)
@@ -43,8 +46,28 @@
 #define LIGHT_BLUE MK_COLOR(_BOLD, _BLACK, _BLUE)
 #define LIGHT_GREEN MK_COLOR(_BOLD, _BLACK, _GREEN)
 #define LIGHT_RED MK_COLOR(_BOLD, _BLACK, _RED)
+*/
+
+#define WHITE MK_COLOR(_NORMAL, _WHITE)
+
+#define DARK_MAGENTA MK_COLOR(_NORMAL, _MAGENTA)
+#define DARK_YELLOW MK_COLOR(_NORMAL,  _YELLOW)
+#define DARK_CYAN MK_COLOR(_NORMAL, _CYAN)
+#define DARK_BLUE MK_COLOR(_NORMAL, _BLUE)
+#define DARK_GREEN MK_COLOR(_NORMAL, _GREEN)
+
+#define LIGHT_MAGENTA MK_COLOR(_BOLD, _MAGENTA)
+#define LIGHT_YELLOW MK_COLOR(_BOLD, _YELLOW)
+#define LIGHT_CYAN MK_COLOR(_BOLD, _CYAN)
+#define LIGHT_BLUE MK_COLOR(_BOLD, _BLUE)
+#define LIGHT_GREEN MK_COLOR(_BOLD, _GREEN)
+#define LIGHT_RED MK_COLOR(_BOLD,  _RED)
+
+#undef DEBUG
+#define DEBUG 0
 
 #if DEBUG > 0
+	#define WITH_COLOR
 
   #ifdef WITH_COLOR
     #define PRINT_DEBUG(symb, level, color, ...) \
@@ -52,16 +75,19 @@
           fprintf(stderr, "%s%s%s", ESC, color, END); \
           fprintf(stderr, __VA_ARGS__); \
           fprintf(stderr, "%s%s%s", ESC, COLOR_NORMAL, END); \
+          fflush(stderr);\
       }
   #else
     #define PRINT_DEBUG(symb, level, color, ...) \
       if (symb > level) { \
-        fprintf(stderr, __VA_ARGS__); \
+        fprintf(stderr, __VA_ARGS__);\
+		fflush(stderr);\
       }
   #endif
 
 #else
-#define PRINT_DEBUG(symb, level, color, args...) do {} while(0);
+	#define PRINT_DEBUG(symb, level, color, args...) do {} while(0);
+
 #endif
 
 #define UNIMPLEMENTED() {                           \
