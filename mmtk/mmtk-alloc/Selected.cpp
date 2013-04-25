6279c2f59f1cef619abdf07424d523ee84ee0e57
Peter Senna Tschudin <peter.senna@gmail.com>
2013-04-25 17:14:23 +0000

    MMTkMutatorAllocate() allocated object should not be given to llvm_gcroot().(cherry picked from commit e32d14817cf26796bf5010f7a472c80f94cfd5a6)
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@180474 91177308-0d34-0410-b5e6-96231b3b80d8

--- mmtk/mmtk-alloc/Selected.cpp
+++ mmtk/mmtk-alloc/Selected.cpp
@@ -293,13 +293,11 @@ void Collector::initialise(int argc, char** argv) {
 }
 
 extern "C" void* MMTkMutatorAllocate(uint32_t size, void* type) {
-  gc* val = NULL;
   gcHeader* head = NULL;
-  llvm_gcroot(val, 0);
   size += gcHeader::hiddenHeaderSize();
   size = llvm::RoundUpToAlignment(size, sizeof(void*));
   head = (gcHeader*)MutatorThread::get()->Allocator.Allocate(size);
-  val = head->toReference();
+  void* val = head->toReference();
   vmkit::Thread::get()->MyVM->setType(val, type);
   return val;
 }
