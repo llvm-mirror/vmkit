9ed3f3c17e32dd6fe0ae65626a5337f09bf7972d
Peter Senna Tschudin <peter.senna@gmail.com>
2013-04-25 17:14:58 +0000

    Updated J3 native interface. (cherry picked from commit e31a93eb05a86860cbe0f110dbfda5caee8a3e81)
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/trunk@180480 91177308-0d34-0410-b5e6-96231b3b80d8

--- incinerator/osgi/src/j3/vm/OSGi.java
+++ incinerator/osgi/src/j3/vm/OSGi.java
@@ -2,7 +2,11 @@ package j3.vm;
 
 public class OSGi
 {
+	// OSGi hooks and information gathering
 	public static native void associateBundleClass(long bundleID, Class classObject);
+	public static native void notifyBundleUninstalled(long bundleID);
+	
+	// Commands
 	public static native void resetReferencesToBundle(long bundleID);
 	public static native void dumpClassLoaderBundles();
 }
