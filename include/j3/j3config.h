1a1fb2344e46edebb4a487f65f449055b4f75d6e
Gael Thomas <gael.thomas@lip6.fr>
2013-11-25 09:38:58 +0000

    remove generated config.h file from the repo
    git-svn-id: https://llvm.org/svn/llvm-project/vmkit/branches/mcjit@195630 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/j3/j3config.h
+++
@@ -1,6 +0,0 @@
-
-#define SHLIBEXT      ".dylib"
-#define OPENJDK_HOME  "/Users/gthomas/research/vmkit4/jdk8/build/macosx-x86_64-normal-server-release/images/j2sdk-bundle/jdk1.8.0.jdk/Contents/Home/"
-
-
-
